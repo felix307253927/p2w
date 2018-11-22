757a5e5aeea0df56666c1a453489c21e38eadd97
felix <307253927@qq.com>
2018-11-22 16:17:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const suf  = /\.(wav|pcm)$/
 const wav  = /\.wav$/
 const prex = /\\|\//g
 
-async function readFile(dPath, rate = 16000, bit = 16, channel = 1) {
+async function readFile(dPath, audio, rate = 16000, bit = 16, channel = 1) {
   if (+rate >= 8000 || +bit >= 2 || +channel >= 1) {
     const files = fs.readdirSync(dPath)
     for (let i = 0, len = files.length; i < len; i++) {
@@ -26,19 +26,20 @@ async function readFile(dPath, rate = 16000, bit = 16, channel = 1) {
       if (stat.isDirectory()) {
         await readFile(p, rate, bit, channel)
       } else if (stat.isFile()) {
-        const name = f.toLowerCase()
-        if (suf.test(name)) {
-          const fPath = path.join(dPath, name.replace(wav, '_org.pcm'))
-          fs.renameSync(p, fPath)
+        // const name = f.toLowerCase()
+        // if (suf.test(name)) {
+        if (f === audio) {
+          const fPath = path.join(dPath, `${f}.wav`)
+          // fs.renameSync(p, fPath)
           await new Promise((resolve, reject) => {
-            fs.createReadStream(fPath).pipe(new WStream({
+            fs.createReadStream(p).pipe(new WStream({
               bufSize     : stat.size,
               sampleRate  : rate,
               sampleBits  : bit,
               channelCount: channel
-            })).pipe(fs.createWriteStream(p))
+            })).pipe(fs.createWriteStream(fPath))
               .on("close", () => {
-                fs.unlinkSync(fPath)
+                // fs.unlinkSync(fPath)
                 resolve()
               })
               .on('error', (err) => {
@@ -123,6 +124,7 @@ const args = process.argv.slice(2)
 if (!args.length) {
   console.log('请输入参数 如: -d="文件目录地址" -r=16000 -b=16 -c=1')
   console.log('\t-d 文件目录地址 必须')
+  console.log('\t-n 操作文件名 [不可与 -ex 参数一起使用]')
   console.log('\t-r 采样率 默认 16000 可不传')
   console.log('\t-b 比特率 默认 16 可不传')
   console.log('\t-c 声道数 默认 1  可不传')
@@ -143,8 +145,12 @@ if (!args.length) {
     if (stat.isDirectory()) {
       console.log("指定目录:", arg.d)
       if (!arg.ex) {
-        console.log("参数:", "采样率:", arg.r || 16000, "比特率:", arg.b || 16, "声道:", arg.c || 1)
-        readFile(arg.d, arg.r, arg.b, arg.c)
+        if(!arg.n){
+          console.log("参数 -n 音频文件名 是必须的")
+        } else {
+          console.log("参数:", "采样率:", arg.r || 16000, "比特率:", arg.b || 16, "声道:", arg.c || 1)
+          readFile(arg.d, arg.n, arg.r, arg.b, arg.c)
+        }
       } else {
         extractAudio(arg.d)
       }
